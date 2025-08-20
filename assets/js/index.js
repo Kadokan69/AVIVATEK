d8e3f16df0cf9f2f3a9d73ca4f22dd5f29d57484
Andrey Kelorev <kelorev.andrey@yandex.ru>
2025-08-20 17:53:41 +0300

    feat: Доработана верста под планшеты. Добавлен скрип на отслеживание позиции курсора в грантиях.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,53 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.getElementById("burger-menu");
+  const nav = document.getElementById("nav-menu");
+  const cross = document.getElementById("cross-menu");
+  const overlay = document.getElementById("menu-overlay");
 
-document.addEventListener('DOMContentLoaded', function() {
-	const burger = document.getElementById('burger-menu');
-	const nav = document.getElementById('nav-menu');
-	const cross = document.getElementById('cross-menu');
-	const overlay = document.getElementById('menu-overlay');
+  function openMenu() {
+    nav.classList.add("open");
+    burger.classList.add("active");
+    overlay.classList.add("open");
+  }
+  function closeMenu() {
+    nav.classList.remove("open");
+    burger.classList.remove("active");
+    overlay.classList.remove("open");
+  }
 
-	function openMenu() {
-		nav.classList.add('open');
-		burger.classList.add('active');
-		overlay.classList.add('open');
-	}
-	function closeMenu() {
-		nav.classList.remove('open');
-		burger.classList.remove('active');
-		overlay.classList.remove('open');
-	}
+  burger.addEventListener("click", function (e) {
+    e.stopPropagation();
+    if (nav.classList.contains("open")) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  });
+  cross.addEventListener("click", function (e) {
+    e.stopPropagation();
+    closeMenu();
+  });
+  overlay.addEventListener("click", function () {
+    closeMenu();
+  });
+  // При клике вне меню — закрыть
+  document.addEventListener("click", function (e) {
+    if (!nav.contains(e.target) && !burger.contains(e.target)) {
+      closeMenu();
+    }
+  });
+  const element = document.querySelector(".guarantee");
+  element.addEventListener("mousemove", function (e) {
+    const x = e.clientX;
+    const y = e.clientY;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const item = document.querySelector(".guarantee__img");
 
-	burger.addEventListener('click', function(e) {
-		e.stopPropagation();
-		if (nav.classList.contains('open')) {
-			closeMenu();
-		} else {
-			openMenu();
-		}
-	});
-	cross.addEventListener('click', function(e) {
-		e.stopPropagation();
-		closeMenu();
-	});
-	overlay.addEventListener('click', function() {
-		closeMenu();
-	});
-	// При клике вне меню — закрыть
-	document.addEventListener('click', function(e) {
-		if (!nav.contains(e.target) && !burger.contains(e.target)) {
-			closeMenu();
-		}
-	});
+    if (window.innerWidth > 958) {
+      item.style.transform = `translate(calc(-50% - ${((x - width / 2) / 100) * 1.85}px), calc(-50% - ${
+        ((y - height / 2) / 100) * 1.85
+      }px))`;
+    }
+  });
 });
